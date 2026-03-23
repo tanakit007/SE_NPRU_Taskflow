8503d4a3aae31006032fcd2d6a6ef280cb148758
tanakit007 <156067811+tanakit007@users.noreply.github.com>
2026-03-23 16:48:13 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 // backend/server.js
 app.use(cors({
-  origin: "http://localhost:5173", // URL ของ Frontend
+  origin: process.env.CLIENT_URL, // URL ของ Frontend
   credentials: true, // อนุญาตให้ส่ง Cookie (JWT)
 }));
 
